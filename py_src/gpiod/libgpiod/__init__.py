5edf84f3ebea92f8787479911a179b34856cf021
Hyeonki Hong <hhk7734@gmail.com>
2020-04-13 12:43:52 +0900

    gpiod: start v1.0

--- py_src/gpiod/libgpiod/__init__.py
+++ py_src/gpiod/libgpiod/__init__.py
@@ -21,3 +21,23 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
+from ctypes import CDLL, c_char_p
+
+libgpiod = CDLL("libgpiod.so")
+
+
+def wrap_libgpiod_func(name: str, argtypes: list, restype):
+    func = libgpiod[name]
+    func.argtypes = argtypes
+    func.restype = restype
+    return func
+
+
+gpiod_version_string = wrap_libgpiod_func(
+    "gpiod_version_string",
+    None,
+    c_char_p
+)
+
+
+__version__ = gpiod_version_string().decode()
--- setup.py
+++ setup.py
@@ -22,131 +22,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
-import sys
-import setuptools
+from setuptools import setup
 from os import path
 
 BASE_DIR = path.dirname(path.abspath(__file__))
 CHANGELOG_PATH = path.join(BASE_DIR, "CHANGELOG")
-
-LIBGPIODCXX_VERSION_CODE = 0
-
-if sys.argv[1] not in ("clean", "egg_info", "sdist"):
-    import pkgconfig
-
-    if not pkgconfig.exists('libgpiodcxx'):
-        raise OSError("\n\n\tpkg-config: Failed to find libgpiodcxx.\n")
-
-    _libgpiodcxx_version = pkgconfig.modversion('libgpiodcxx')
-
-    LIBGPIODCXX_VERSION_CODE = (
-        int(_libgpiodcxx_version[0]) << 8) + int(_libgpiodcxx_version[2])
-
-    if LIBGPIODCXX_VERSION_CODE < ((1 << 8) + 1):
-        raise OSError("\n\n\tgpiod: Supports version 1.1 or higher.\n")
-
-
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
-
-
-ext_modules = [
-    Extension(
-        "gpiod._gpiod",
-        sources=["c_src/gpiod.cpp"],
-        include_dirs=[
-            # Path to pybind11 headers
-            get_pybind_include(),
-            get_pybind_include(user=True)
-        ],
-        language="c++"
-    ),
-]
 
 with open(CHANGELOG_PATH, "r") as f:
     version = f.readline()
     version = version.split()
     version = version[1][1:-1]
 
-
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14/17] compiler flag.
-    The newer version is prefered over c++11 (when it is available).
-    """
-    flags = ['-std=c++17', '-std=c++14']
-
-    for flag in flags:
-        if has_flag(compiler, flag):
-            return flag
-
-    raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                       'is needed!')
-
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': ["-lgpiodcxx",
-                 f"-DLIBGPIODCXX_VERSION_CODE={LIBGPIODCXX_VERSION_CODE}"],
-    }
-    l_opts = {
-        'msvc': [],
-        'unix': ["-lgpiodcxx"],
-    }
-
-    if sys.platform == 'darwin':
-        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        c_opts['unix'] += darwin_opts
-        l_opts['unix'] += darwin_opts
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        link_opts = self.l_opts.get(ct, [])
-        if ct == 'unix':
-            opts.append('-DVERSION_INFO="%s"' %
-                        self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"' %
-                        self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-            ext.extra_link_args = link_opts
-        build_ext.build_extensions(self)
-
-
 setup(
     version=version,
-    ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExt},
 )

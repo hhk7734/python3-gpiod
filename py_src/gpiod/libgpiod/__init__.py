cd7d9f5a7274acfb445fa6c3f1a36e627d8815aa
Hyeonki Hong <hhk7734@gmail.com>
2020-04-28 19:50:18 +0900

    libgpiod: remove ctypes data type and CDLL

--- py_src/gpiod/libgpiod/__init__.py
+++ py_src/gpiod/libgpiod/__init__.py
@@ -21,19 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from ctypes import (
-    CDLL,
-    c_bool,
-    c_char,
-    c_int,
-    c_uint,
-    c_long,
-    c_char_p,
-    POINTER,
-    pointer,
-    set_errno,
-    Structure,
-)
+from ctypes import set_errno
 from errno import ENODEV, ENOENT, ENOTTY
 from fcntl import ioctl
 from os import (
@@ -50,116 +38,72 @@ from os import (
 )
 from os.path import basename
 from stat import S_ISCHR
-from typing import List
 
 from .time_h import timespec
 from .gpio_h import gpiochip_info, GPIO_GET_CHIPINFO_IOCTL
-
-SO_CANDIDATE = (
-    "libgpiod.so",
-    "libgpiod.so.2",
-    "libgpiod.so.1",
-)
-libgpiod = None
-
-for so in SO_CANDIDATE:
-    try:
-        libgpiod = CDLL(so, use_errno=True)
-        break
-    except OSError:
-        pass
-    except BaseException as error:
-        raise error
-
-if libgpiod is None:
-    raise FileNotFoundError("Failed to find any of {}".format(SO_CANDIDATE))
-
-
-def wrap_libgpiod_func(name: str, argtypes: list, restype):
-    # pylint: disable=missing-function-docstring
-    func = libgpiod[name]
-    func.argtypes = argtypes
-    func.restype = restype
-    return func
-
-
-gpiod_version_string = wrap_libgpiod_func(
-    "gpiod_version_string", None, c_char_p
-)
-
-
-__version__ = tuple(int(x) for x in gpiod_version_string().split(b"."))
-
-if __version__ < (1, 1):
-    ENUM_BASE = 0
-else:
-    ENUM_BASE = 1
-
 
 # pylint: disable=too-few-public-methods
 
 
 # Forward declaration
-class gpiod_chip(Structure):
+class gpiod_chip:
     pass
 
 
 # Forward declaration
-class gpiod_line(Structure):
+class gpiod_line:
     pass
 
 
 # Forward declaration
-class gpiod_line_bulk(Structure):
+class gpiod_line_bulk:
     pass
 
 
 GPIOD_LINE_BULK_MAX_LINES = 64
 
 
-class gpiod_line_bulk(Structure):
+class gpiod_line_bulk:
     # pylint: disable=function-redefined
-    _fields_ = [
-        ("lines", POINTER(gpiod_line) * GPIOD_LINE_BULK_MAX_LINES),
-        ("num_lines", c_uint),
-    ]
+    def __init__(self):
+        # GPIOD_LINE_BULK_MAX_LINES
+        self.lines = []
+        self.num_lines = 0
 
 
-GPIOD_LINE_DIRECTION_INPUT = ENUM_BASE
-GPIOD_LINE_DIRECTION_OUTPUT = ENUM_BASE + 1
+GPIOD_LINE_DIRECTION_INPUT = 1
+GPIOD_LINE_DIRECTION_OUTPUT = 2
 
-GPIOD_LINE_ACTIVE_STATE_HIGH = ENUM_BASE
-GPIOD_LINE_ACTIVE_STATE_LOW = ENUM_BASE + 1
+GPIOD_LINE_ACTIVE_STATE_HIGH = 1
+GPIOD_LINE_ACTIVE_STATE_LOW = 2
 
-GPIOD_LINE_REQUEST_DIRECTION_AS_IS = ENUM_BASE
-GPIOD_LINE_REQUEST_DIRECTION_INPUT = ENUM_BASE + 1
-GPIOD_LINE_REQUEST_DIRECTION_OUTPUT = ENUM_BASE + 2
-GPIOD_LINE_REQUEST_EVENT_FALLING_EDGE = ENUM_BASE + 3
-GPIOD_LINE_REQUEST_EVENT_RISING_EDGE = ENUM_BASE + 4
-GPIOD_LINE_REQUEST_EVENT_BOTH_EDGES = ENUM_BASE + 5
+GPIOD_LINE_REQUEST_DIRECTION_AS_IS = 1
+GPIOD_LINE_REQUEST_DIRECTION_INPUT = 2
+GPIOD_LINE_REQUEST_DIRECTION_OUTPUT = 3
+GPIOD_LINE_REQUEST_EVENT_FALLING_EDGE = 4
+GPIOD_LINE_REQUEST_EVENT_RISING_EDGE = 5
+GPIOD_LINE_REQUEST_EVENT_BOTH_EDGES = 6
 
 GPIOD_LINE_REQUEST_FLAG_OPEN_DRAIN = 0b001
 GPIOD_LINE_REQUEST_FLAG_OPEN_SOURCE = 0b010
 GPIOD_LINE_REQUEST_FLAG_ACTIVE_LOW = 0b100
 
 
-class gpiod_line_request_config(Structure):
-    _fields_ = [
-        ("consumer", c_char_p),
-        ("request_type", c_int),
-        ("flags", c_int),
-    ]
+class gpiod_line_request_config:
+    def __init__(self):
+        self.consumer = ""
+        self.request_type = 0
+        self.flags = 0
 
 
-GPIOD_LINE_EVENT_RISING_EDGE = ENUM_BASE
-GPIOD_LINE_EVENT_FALLING_EDGE = ENUM_BASE + 1
+GPIOD_LINE_EVENT_RISING_EDGE = 1
+GPIOD_LINE_EVENT_FALLING_EDGE = 2
 
 
-class gpiod_line_event(Structure):
-    _fields_ = [
-        ("ts", timespec),
-        ("event_type", c_int),
-    ]
+class gpiod_line_event:
+    def __init__(self):
+        self.ts = timespec()
+        self.event_type = 0
 
 
 # core.c
@@ -168,61 +112,42 @@ LINE_FREE = 0
 LINE_REQUESTED_VALUES = 1
 LINE_REQUESTED_EVENTS = 2
 
-if __version__ < (1, 0, 1):
 
-    class gpiod_line(Structure):
-        # pylint: disable=function-redefined
-        _fields_ = [
-            ("offset", c_uint),
-            ("direction", c_int),
-            ("active_state", c_int),
-            ("used", c_bool),
-            ("open_source", c_bool),
-            ("open_drain", c_bool),
-            ("state", c_int),
-            ("up_to_date", c_bool),
-            ("chip", POINTER(gpiod_chip)),
-            ("fd", c_int),
-            ("name", c_char * 32),
-            ("consumer", c_char * 32),
-        ]
+class line_fd_handle:
+    def __init__(self):
+        self.fd = 0
+        self.refcount = 0
 
 
-else:
-
-    class line_fd_handle(Structure):
-        _fields_ = [
-            ("fd", c_int),
-            ("refcount", c_int),
-        ]
-
-    class gpiod_line(Structure):
-        # pylint: disable=function-redefined
-        _fields_ = [
-            ("offset", c_uint),
-            ("direction", c_int),
-            ("active_state", c_int),
-            ("used", c_bool),
-            ("open_source", c_bool),
-            ("open_drain", c_bool),
-            ("state", c_int),
-            ("up_to_date", c_bool),
-            ("chip", POINTER(gpiod_chip)),
-            ("fd_handle", POINTER(line_fd_handle)),
-            ("name", c_char * 32),
-            ("consumer", c_char * 32),
-        ]
+class gpiod_line:
+    # pylint: disable=function-redefined, too-many-instance-attributes
+    def __init__(self):
+        self.offset = 0
+        self.direction = 0
+        self.active_state = 0
+        self.used = False
+        self.open_source = False
+        self.open_drain = False
+        self.state = 0
+        self.up_to_date = False
+        self.chip = gpiod_chip()
+        self.fd_handle = line_fd_handle()
+        # size 32
+        self.name = ""
+        # size 32
+        self.consumer = ""
 
 
-class gpiod_chip(Structure):
+class gpiod_chip:
     # pylint: disable=function-redefined
-    _fields_ = [
-        ("lines", POINTER(POINTER(gpiod_line))),
-        ("num_lines", c_uint),
-        ("fd", c_int),
-        ("name", c_char * 32),
-        ("label", c_char * 32),
-    ]
+    def __init__(self):
+        self.lines = []
+        self.num_lines = 0
+        self.fd = 0
+        # size 32
+        self.name = ""
+        # size 32
+        self.label = ""
 
 
 # Function
@@ -274,16 +199,16 @@ def _is_gpiochip_cdev(path: str) -> bool:
     return True
 
 
-def gpiod_chip_open(path: str) -> POINTER(gpiod_chip):
+def gpiod_chip_open(path: str) -> gpiod_chip:
     """
     @brief Open a gpiochip by path.
 
     @param path: Path to the gpiochip device file.
 
-    @return GPIO chip handle or NULL if an error occurred.
+    @return GPIO chip handle or None if an error occurred.
     """
     info = gpiochip_info()
-    chip = pointer(gpiod_chip())
+    chip = gpiod_chip()
 
     try:
         fd = os_open(path, O_RDWR | O_CLOEXEC)
@@ -301,58 +226,58 @@ def gpiod_chip_open(path: str) -> POINTER(gpiod_chip):
         os_close(fd)
         return None
 
-    chip[0].fd = fd
-    chip[0].num_lines = info.lines
-    chip[0].name = info.name
+    chip.fd = fd
+    chip.num_lines = info.lines
+    chip.name = info.name.decode()
 
     if info.label[0] == "\0":
-        chip[0].label = b"unknown"
+        chip.label = "unknown"
     else:
-        chip[0].label = info.label
+        chip.label = info.label.decode()
 
     return chip
 
 
-def gpiod_chip_close(chip: POINTER(gpiod_chip)):
+def gpiod_chip_close(chip: gpiod_chip):
     """
     @brief Close a GPIO chip handle and release all allocated resources.
 
     @param chip: The GPIO chip object.
     """
-    if bool(chip[0].lines):
-        for i in range(chip[0].num_lines):
-            line = chip[0].lines[i]
-            if bool(line):
+    if len(chip.lines) > 0:
+        for i in range(chip.num_lines):
+            line = chip.lines[i]
+            if line is not None:
                 gpiod_line_release(line)
 
-    os_close(chip[0].fd)
+    os_close(chip.fd)
     # How to free the chip object?
     del chip
 
 
-def gpiod_chip_name(chip: POINTER(gpiod_chip)) -> str:
+def gpiod_chip_name(chip: gpiod_chip) -> str:
     """
     @brief Get the GPIO chip name as represented in the kernel.
 
     @param chip: The GPIO chip object.
 
-    @return Pointer to a human-readable string containing the chip name.
+    @return String containing the chip name.
     """
-    return chip[0].name.decode()
+    return chip.name
 
 
-def gpiod_chip_label(chip: POINTER(gpiod_chip)) -> str:
+def gpiod_chip_label(chip: gpiod_chip) -> str:
     """
     @brief Get the GPIO chip label as represented in the kernel.
 
     @param chip: The GPIO chip object.
 
-    @return Pointer to a human-readable string containing the chip label.
+    @return String containing the chip label.
     """
-    return chip[0].label.decode()
+    return chip.label
 
 
-def gpiod_chip_num_lines(chip: POINTER(gpiod_chip)) -> int:
+def gpiod_chip_num_lines(chip: gpiod_chip) -> int:
     """
     @brief Get the number of GPIO lines exposed by this chip.
 
@@ -360,10 +285,24 @@ def gpiod_chip_num_lines(chip: POINTER(gpiod_chip)) -> int:
 
     @return Number of GPIO lines.
     """
-    return chip[0].num_lines
+    return chip.num_lines
 
 
-def gpiod_line_is_requested(line: POINTER(gpiod_line)) -> bool:
+def gpiod_chip_get_line(chip: gpiod_chip, offset: int) -> gpiod_line:
+    pass
+
+
+def gpiod_line_request(
+    line: gpiod_line, config: gpiod_line_request_config, default_val: int
+) -> int:
+    pass
+
+
+def gpiod_line_release(line: gpiod_line):
+    pass
+
+
+def gpiod_line_is_requested(line: gpiod_line) -> bool:
     """
     @brief Check if the calling user has ownership of this line.
 
@@ -372,43 +311,69 @@ def gpiod_line_is_requested(line: POINTER(gpiod_line)) -> bool:
     @return True if given line was requested, false otherwise.
     """
     return (
-        line[0].state == LINE_REQUESTED_VALUES
-        or line[0].state == LINE_REQUESTED_EVENTS
+        line.state == LINE_REQUESTED_VALUES
+        or line.state == LINE_REQUESTED_EVENTS
     )
+
+
+def gpiod_line_get_value(line: gpiod_line) -> int:
+    pass
+
+
+def gpiod_line_set_value(line: gpiod_line, value: int) -> int:
+    pass
+
+
+def gpiod_line_event_wait(line: gpiod_line, timeout: timespec) -> int:
+    pass
+
+
+def gpiod_line_event_wait_bulk(
+    bulk: gpiod_line_bulk, timeout: timespec, event_bulk: gpiod_line_bulk
+) -> int:
+    pass
+
+
+def gpiod_line_event_read(line: gpiod_line, event: gpiod_line_event) -> int:
+    pass
+
+
+def gpiod_line_event_get_fd(line: gpiod_line) -> int:
+    pass
 
 
 # helpers.c
 
 
-def gpiod_chip_open_by_name(name: str) -> POINTER(gpiod_chip):
+def gpiod_chip_open_by_name(name: str) -> gpiod_chip:
     """
     @brief Open a gpiochip by name.
 
     @param name: Name of the gpiochip to open.
 
-    @return GPIO chip handle or NULL if an error occurred.
+    @return GPIO chip handle or None if an error occurred.
     """
     return gpiod_chip_open("/dev/" + str(name))
 
 
-def gpiod_chip_open_by_number(num: int) -> POINTER(gpiod_chip):
+def gpiod_chip_open_by_number(num: int) -> gpiod_chip:
     """
     @brief Open a gpiochip by number.
 
     @param num: Number of the gpiochip.
 
-    @return GPIO chip handle or NULL if an error occurred.
+    @return GPIO chip handle or None if an error occurred.
     """
     return gpiod_chip_open("/dev/gpiochip" + str(num))
 
 
-def gpiod_chip_open_by_label(label: str) -> POINTER(gpiod_chip):
+def gpiod_chip_open_by_label(label: str) -> gpiod_chip:
     """
     @brief Open a gpiochip by label.
 
     @param label: Label of the gpiochip to open.
 
-    @return GPIO chip handle or NULL if the chip with given label was not found
+    @return GPIO chip handle or None if the chip with given label was not found
             or an error occured.
 
     @note If the chip cannot be found but no other error occurred, errno is set
@@ -429,13 +394,13 @@ def gpiod_chip_open_by_label(label: str) -> POINTER(gpiod_chip):
     return None
 
 
-def gpiod_chip_open_lookup(descr) -> POINTER(gpiod_chip):
+def gpiod_chip_open_lookup(descr) -> gpiod_chip:
     """
     @brief Open a gpiochip based on the best guess what the path is.
 
     @param descr: String describing the gpiochip.
 
-    @return GPIO chip handle or NULL if an error occurred.
+    @return GPIO chip handle or None if an error occurred.
 
     This routine tries to figure out whether the user passed it the path to the
     GPIO chip, its name, label or number as a string. Then it tries to open it
@@ -456,6 +421,10 @@ def gpiod_chip_open_lookup(descr) -> POINTER(gpiod_chip):
         return gpiod_chip_open(descr)
 
     return chip
+
+
+def gpiod_chip_find_line(chip: gpiod_chip, name: str) -> gpiod_line:
+    pass
 
 
 # iter.c
@@ -516,52 +485,3 @@ class gpiod_chip_iter:
             return self.chips[index]
 
         raise StopIteration
-
-
-gpiod_chip_get_line = wrap_libgpiod_func(
-    "gpiod_chip_get_line", [POINTER(gpiod_chip), c_uint,], POINTER(gpiod_line)
-)
-
-gpiod_chip_find_line = wrap_libgpiod_func(
-    "gpiod_chip_find_line",
-    [POINTER(gpiod_chip), c_char_p,],
-    POINTER(gpiod_line),
-)
-
-gpiod_line_request = wrap_libgpiod_func(
-    "gpiod_line_request",
-    [POINTER(gpiod_line), POINTER(gpiod_line_request_config), c_int,],
-    c_int,
-)
-
-gpiod_line_release = wrap_libgpiod_func(
-    "gpiod_line_release", [POINTER(gpiod_line),], None
-)
-
-gpiod_line_get_value = wrap_libgpiod_func(
-    "gpiod_line_get_value", [POINTER(gpiod_line),], c_int
-)
-
-gpiod_line_set_value = wrap_libgpiod_func(
-    "gpiod_line_set_value", [POINTER(gpiod_line), c_int,], c_int
-)
-
-gpiod_line_event_wait = wrap_libgpiod_func(
-    "gpiod_line_event_wait", [POINTER(gpiod_line), POINTER(timespec),], c_int
-)
-
-gpiod_line_event_read = wrap_libgpiod_func(
-    "gpiod_line_event_read",
-    [POINTER(gpiod_line), POINTER(gpiod_line_event),],
-    c_int,
-)
-
-gpiod_line_event_wait_bulk = wrap_libgpiod_func(
-    "gpiod_line_event_wait_bulk",
-    [POINTER(gpiod_line_bulk), POINTER(timespec), POINTER(gpiod_line_bulk),],
-    c_int,
-)
-
-gpiod_line_event_get_fd = wrap_libgpiod_func(
-    "gpiod_line_event_get_fd", [POINTER(gpiod_line),], c_int
-)

22eba6496e9e9df7e24746bfa1106103614b9fbe
Hyeonki Hong <hhk7734@gmail.com>
2020-12-26 04:47:16 +0900

    test: add tests for bias flags

--- py_src/gpiod/test/blink.py
+++ py_src/gpiod/test/blink.py
@@ -43,11 +43,23 @@ print("is line open drain: ", led.is_open_drain())
 print("is_open_source: ", led.is_open_source())
 print("is line requested: ", led.is_requested())
 
+bias = led.bias
+if bias == line.BIAS_AS_IS:
+    print("line bias: AS_IS")
+elif bias == line.BIAS_DISABLE:
+    print("line bias: DISABLE")
+elif bias == line.BIAS_PULL_UP:
+    print("line bias: PULL_UP")
+elif bias == line.BIAS_PULL_DOWN:
+    print("line bias: PULL_DOWN")
+
+
 print("\nrequest line\n")
 
 config = line_request()
 config.consumer = "Blink"
 config.request_type = line_request.DIRECTION_OUTPUT
+config.flags = line_request.FLAG_BIAS_PULL_UP
 
 led.request(config)
 
@@ -64,6 +76,16 @@ print("is line used: ", led.is_used())
 print("is line open drain: ", led.is_open_drain())
 print("is_open_source: ", led.is_open_source())
 print("is line requested: ", led.is_requested())
+
+bias = led.bias
+if bias == line.BIAS_AS_IS:
+    print("line bias: AS_IS")
+elif bias == line.BIAS_DISABLE:
+    print("line bias: DISABLE")
+elif bias == line.BIAS_PULL_UP:
+    print("line bias: PULL_UP")
+elif bias == line.BIAS_PULL_DOWN:
+    print("line bias: PULL_DOWN")
 
 while True:
     led.set_value(0)
